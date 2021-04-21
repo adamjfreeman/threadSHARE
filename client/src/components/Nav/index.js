18dde2e679ef67379febe11a4ce45ea46172303c
Fire Dragon <daniwhitlock4@gmail.com>
2021-04-20 18:34:49 -0600

    added initial file and folder structure and package.json files

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,1 @@
+// Put in the images of the items we are selling
---
+++ server/config/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+console.log(process.env.MONGODB_URI);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+module.exports = mongoose.connection;
