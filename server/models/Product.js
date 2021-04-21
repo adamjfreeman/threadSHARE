92bf7a79113f497a8ebfeb15ceac9c7ead0fcdf3
mheyn95 <mheyn95@gmail.com>
2021-04-21 14:10:27 -0600

    initial product model

---
+++ server/models/Product.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  text: {
+    type: String,
+  },
+  size: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0.99,
+  },
+  quantity: {
+    type: Number,
+    min: 0,
+    default: 0,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
