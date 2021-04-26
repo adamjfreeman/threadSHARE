7a8b2d65b49068e1959eabb64e09213bea22a6c6
Fire Dragon <daniwhitlock4@gmail.com>
2021-04-25 20:10:29 -0600

    added reducers, app.js, store

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,76 @@
+import { NoFragmentCyclesRule } from "graphql";
+import {
+    UPDATE_PRODUCTS,
+    ADD_TO_CART,
+    ADD_MULTIPLE_TO_CART,
+    REMOVE_FROM_CART,
+    UPDATE_CART_QUANTITY,
+    CLEAR_CART,
+    TOGGLE_CART
+} from "./actions";
+
+const initialState = {
+    product: [],
+    cart: [],
+    cartOpen: false
+};
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case UPDATE_PRODUCTS:
+            return {
+                ...state,
+                product: [...action.products],
+            };
+        case ADD_TO_CART:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: [...state.cart, action.product],
+            };
+
+        case ADD_MULTIPLE_TO_CART:
+            return {
+                ...state,
+                cart: [...state.cart, ...action.products],
+            };
+
+        case REMOVE_FROM_CART:
+            let newState = state.cart.filter(product => {
+                return product._id !== action._id;
+            });
+
+            return {
+                ...state,
+                cartOpen: newState.length > 0,
+                cart: newState
+            };
+
+        case UPDATE_CART_QUANTITY:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: state.cart.map(product => {
+                    if (action._id === product._id) {
+                        product.purchaseQuantity = action.purchaseQuantity
+                    }
+                    return product
+                })
+            };
+
+        case CLEAR_CART:
+            return {
+                ...state,
+                cartOpen: false,
+                cart: []
+            };
+
+        case TOGGLE_CART:
+            return {
+                ...state,
+                cartOpen: !state.cartOpen
+            };
+    }
+};
+
+export default reducer;
