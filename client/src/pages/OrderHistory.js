3080dda08543d3b2541b0f2a7a132c352a7e0104
Fire Dragon <daniwhitlock4@gmail.com>
2021-04-25 20:39:03 -0600

    added orderhistory code

--- client/src/pages/OrderHistory.js
+++ client/src/pages/OrderHistory.js
@@ -1,1 +1,26 @@
 // past designs- order history
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_USER } from "../utils/queries";
+
+function OrderHistory() {
+    // TO DO: MAKE SURE THE CORRECT QUERY IS BEING USED
+    const { data } = useQuery(QUERY_USER);
+    let user;
+  
+    if (data) {
+      user = data.user;
+    }
+
+    return (
+        <div>
+            <h2>Order history for {user.firstName} </h2>
+            {/* Need to input the rest of the code for objects from order history */}
+        </div>
+    )
+};
+export default OrderHistory;
+  
