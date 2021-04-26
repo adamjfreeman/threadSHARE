deab6ae568ad3ff3398b897aff1f7492f16760ab
Fire Dragon <daniwhitlock4@gmail.com>
2021-04-25 20:46:42 -0600

    working on creating groups for products

---
+++ client/src/components/ProductsSmall/index.js
@@ -0,0 +1,3 @@
+// This is where you will put the code that displays the shirt/sweatshirt categories
+// similar to the nav bar, but on the bottom
+// links will take you to the specific pages for the items
