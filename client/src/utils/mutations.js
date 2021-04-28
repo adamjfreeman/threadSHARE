bdb2a116ab3b2694e2d1a17eeacea9ed060abc53
mheyn95 <mheyn95@gmail.com>
2021-04-27 19:22:54 -0600

    added all mutations and all queries

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,6 @@
 // mitchell put in mutations
-import { GraphQLString } from 'graphql';
-import gql from 'graphql-tag';
+import { GraphQLString } from "graphql";
+import gql from "graphql-tag";
 
 // what am I going to put in, what am I spitting out
 
@@ -17,24 +17,63 @@ export const LOGIN = gql`
 
 // double check after playground
 export const ADD_PRODUCT = gql`
-  mutation addProduct($style: String!, $color: String!, $customText: String!, $size: String!, $quantity: Number!) {
-      addProduct(style: $style, color: $color, customText: $customText, size: $size, quantity: $quantity) {
-          product {
-              _id
-          }
+  mutation addProduct(
+    $style: String!
+    $color: String!
+    $customText: String!
+    $size: String!
+    $quantity: Number!
+  ) {
+    addProduct(
+      style: $style
+      color: $color
+      customText: $customText
+      size: $size
+      quantity: $quantity
+    ) {
+      product {
+        _id
       }
+    }
   }
 `;
 
 // add order - FINISH CODE
 export const ADD_ORDER = gql`
- 
-  
+  mutation addOrder($products: [ID]!) {
+    addOrder(products: $products) {
+      _id
+      purchaseDate
+      products {
+        _id
+        price
+      }
+    }
+  }
+`;
+
+export const ADD_DONATION = gql`
+  mutation addDonation($goalHitDate: String!) {
+    addDonation(goalHitDate: $goalHitDate) {
+      _id
+      goalHitDate
+    }
+  }
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($firstName: String!, $lastName: String!, $email: String!, $password: String!) {
-    addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
       token
       user {
         _id
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,69 @@
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 
-export const QUERY_PRODUCT = qgl`
+export const QUERY_PRODUCT = gql`
+  query product($_id: ID!) {
+    product(_id: $_id) {
+      _id
+      customText
+      style
+      color
+      size
+      quantity
+      price
+      category
+    }
+  }
+`;
+
+export const QUERY_ALL_PRODUCTS = gql`
+  query products {
+    products {
+      _id
+      customText
+      style
+      color
+      size
+      quantity
+    }
+  }
+`;
+
+export const QUERY_ALL_ORDERS = gql`
+  query orders {
+    orders {
+      _id
+      purchaseDate
+      products {
+        _id
+        price
+        category
+      }
+    }
+  }
+`;
+
+export const QUERY_ORDER = gql`
+  query order($_id: ID!) {
+    order(_id: $_id) {
+      _id
+      purchaseDate
+      products {
+        _id
+        customText
+        category
+        price
+      }
+    }
+  }
+`;
+
+export const QUERY_ALL_DONATIONS = gql`
+  query donations {
+    donations {
+      _id
+      goalHitDate
+    }
+  }
 `;
 
 // export const QUERY_ALL_PRODUCTS - don't need all products right now
@@ -8,6 +71,16 @@ export const QUERY_PRODUCT = qgl`
 // export const QUERY_CATEGORIES - don't need right now, need more saved for product type
 
 export const QUERY_USER = gql`
+  query user {
+    user {
+      firstName
+      lastName
+      email
+      orders {
+        _id
+      }
+    }
+  }
 `;
 
 export const QUERY_CHECKOUT = `
